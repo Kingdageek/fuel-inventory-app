020630f24b29ec585cad3cbf0163d016285ab814
Nonso Mgbechi <mgbechinonso@gmail.com>
2019-06-06 16:39:43 +0100

    Registered application routes

--- src/router.js
+++ src/router.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import DispensersIndex from './components/DispensersIndex.vue'
+import CreateDispenser from './components/CreateDispenser.vue'
+import EditDispenser from './components/EditDispenser.vue'
+import TanksIndex from './components/TanksIndex.vue'
+import CreateTank from './components/CreateTank.vue'
+import EditTank from './components/EditTank.vue'
+import SalesForm from './components/SalesForm.vue'
+import StockForm from './components/StockForm.vue'
 
 Vue.use(Router)
 
@@ -12,6 +20,56 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/dispensers',
+      name: 'dispensers.index',
+      component: DispensersIndex
+    },
+    {
+      path: '/dispensers/create',
+      name: 'dispensers.create',
+      component: CreateDispenser
+    },
+    {
+      path: '/dispensers/:dispenserId',
+      name: 'dispensers.edit',
+      component: EditDispenser
+    },
+    {
+      path: '/dispensers/:dispenserId/sales',
+      name: 'dispensers.sales.index',
+      component: CreateDispenser
+    },
+    {
+      path: '/dispensers/:dispenserId/sales/create',
+      name: 'dispensers.sales.create',
+      component: SalesForm
+    },
+    {
+      path: '/tanks',
+      name: 'tanks.index',
+      component: TanksIndex
+    },
+    {
+      path: '/tanks/create',
+      name: 'tanks.create',
+      component: CreateTank
+    },
+    {
+      path: '/tanks/:tankId',
+      name: 'tanks.edit',
+      component: EditTank
+    },
+    {
+      path: '/tanks/:tankId/stock',
+      name: 'tanks.stock.index',
+      component: CreateTank
+    },
+    {
+      path: '/tanks/:tankId/stock/create',
+      name: 'tanks.stock.create',
+      component: StockForm
     },
     {
       path: '/about',
