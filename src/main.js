d306075c597c6ee2606e4f05f92143108fc10c19
Nonso Mgbechi <mgbechinonso@gmail.com>
2019-06-06 16:39:13 +0100

    Added axios, api base url to Vue prototype so they'd be on all instances

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Dashboard from './components/Dashboard'
 import router from './router'
+import axios from 'axios'
+import { API_BASE_URL } from './config.js'
 
 Vue.config.productionTip = false
 
+Vue.prototype.$http = axios
+Vue.prototype.$baseUri = API_BASE_URL
+
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(Dashboard)
 }).$mount('#app')
